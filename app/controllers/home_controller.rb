966e3242913edd1a5c4b4e83db8a17b33da3f46c
ZharkoBlazheski <zarkoblazeski@gmail.com>
2022-08-12 21:58:09 +0200

    Contact form created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,8 +8,24 @@ class HomeController < ApplicationController
   end
 
   def contact
+    @contact = Contact.new()
+  end
+
+  def create_contact
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to root_path, notice: "Contact was successfully established."
+    else
+      render :contact
+    end
   end
 
   def privacy
   end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:full_name, :email, :message)
+    end
+
 end

3a65eb4bc23b190709db3d4839975982a22cbb80
ZharkoBlazheski <zarkoblazeski@gmail.com>
2022-08-15 21:28:21 +0200

    rubocop corrected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
- before_action :authenticate_user!
+  before_action :authenticate_user!
 
-  before_action :configure_permitted_parameters, if:  :devise_controller?
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-    def configure_permitted_parameters
-
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone, :address])
-
-     end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name phone address])
+  end
 end
-

d54d112378b280de6b8670e70d2d8d7355471478
ZharkoBlazheski <zarkoblazeski@gmail.com>
2022-08-16 15:47:27 +0200

    One capital letter

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject(:User) do
+  subject(:user) do
     described_class.create!(first_name: 'Zharko',
                             last_name: 'Blazheski',
                             phone: '071 298 228',

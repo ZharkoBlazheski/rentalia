ee166f3a4179b8a1c4ea68535bc11099a8ebe834
ZharkoBlazheski <zarkoblazeski@gmail.com>
2022-08-16 15:36:11 +0200

    User attributes test added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  subject(:User) do
+    described_class.create!(first_name: 'Zharko',
+                            last_name: 'Blazheski',
+                            phone: '071 298 228',
+                            address: 'Velgoshti')
+
+    describe 'validations' do
+      it { expect(described_class).to be_valid }
+      it { expect(described_class).to validate_presence_of(:first_name) }
+      it { expect(described_class).to validate_presence_of(:last_name) }
+      it { expect(described_class).to validate_presence_of(:phone) }
+      it { expect(described_class).to validate_presence_of(:address) }
+    end
+  end
 end

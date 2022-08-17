652e70432a73a57b22df881494b0bf7f6b96b31e
ZharkoBlazheski <zarkoblazeski@gmail.com>
2022-08-17 20:38:53 +0200

    Added Apartment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_191159) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_174753) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "apartments", force: :cascade do |t|
+    t.string "address"
+    t.string "phone"
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "rooms", default: 1
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "full_name"

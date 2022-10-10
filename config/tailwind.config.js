f2306e3d7d5f387dfc1371cd876000d7f5d97294
orcemaceski <orce@kennedytech.mk>
2022-10-10 22:51:33 +0200

    Added City to apartments

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      width: {
+        '30': '7.5rem',
+      },
     },
   },
   plugins: [
--- spec/factories/apartments.rb
+++ spec/factories/apartments.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :apartment do
     association :user
     address { Faker::Address.street_address }
+    city { Faker::Address.city }
     phone { Faker::PhoneNumber.cell_phone_in_e164 }
     latitude { Faker::Number.decimal(l_digits: 2, r_digits: 7) * [-1, 1].sample }
     longitude { Faker::Number.decimal(l_digits: 2, r_digits: 7) * [-1, 1].sample }

f0da08fcdd6a0f868ea319e51fb19ecd0a1789ed
orcemaceski <maceskiorce@gmail.com>
2022-10-12 16:21:42 +0200

    Edit user information fe

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,5 +3,11 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
     layout 'dashboard', only: [:edit]
+
+    protected
+
+    def update_resource(resource, params)
+      resource.update_without_password(params)
+    end
   end
 end
